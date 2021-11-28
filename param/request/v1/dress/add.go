77727944eb9eb94cf970c61f89cbe1749c6341eb
rayallen20 <452857163@qq.com>
2021-11-28 17:18:50 +0800

    fix bug:添加已有品类礼服时 请求参数中的副图为可选参数

--- param/request/v1/dress/add.go
+++ param/request/v1/dress/add.go
@@ -27,7 +27,7 @@ type Dress struct {
 	// CoverImg 封面图
 	CoverImg string `form:"coverImg" binding:"gt=0,required,imgUrl" errField:"coverImg"`
 	// SecondaryImg 副图
-	SecondaryImg []string `form:"secondaryImg" binding:"gt=0,lte=1,imgUrls" errField:"secondaryImg"`
+	SecondaryImg []string `form:"secondaryImg" binding:"gte=0,lte=1,imgUrls" errField:"secondaryImg"`
 }
 
 func (a *AddParam) Bind(c *gin.Context) error {
