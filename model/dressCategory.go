2767f4bd59063b82d03ead73a9728df21e983fa3
rayallen20 <452857163@qq.com>
2022-05-02 20:18:57 +0800

    fix bug:更新品类信息时副图字段若不传则不更新,已修复

--- model/dressCategory.go
+++ model/dressCategory.go
@@ -9,31 +9,31 @@ import (
 // onSale:在售
 // soldOut:脱销
 // haltSales:下架
-var CategoryStatus = map[string]string {
-	"onSale":"onSale",
-	"soldOut":"soldOut",
-	"haltSales":"haltSales",
+var CategoryStatus = map[string]string{
+	"onSale":    "onSale",
+	"soldOut":   "soldOut",
+	"haltSales": "haltSales",
 }
 
 // DressCategory dress_category表的ORM
 type DressCategory struct {
-	Id int
-	KindId int
-	Kind *DressKind `gorm:"foreignKey:KindId"`
-	SerialNumber string
-	Quantity int
+	Id               int
+	KindId           int
+	Kind             *DressKind `gorm:"foreignKey:KindId"`
+	SerialNumber     string
+	Quantity         int
 	RentableQuantity int
-	CharterMoney int
-	AvgCharterMoney int
-	CashPledge int
-	RentCounter int
-	LaundryCounter int
-	MaintainCounter int
-	CoverImg string
-	SecondaryImg string `gorm:"text"`
-	Status string
-	CreatedTime time.Time `gorm:"autoCreateTime"`
-	UpdatedTime time.Time `gorm:"autoUpdateTime"`
+	CharterMoney     int
+	AvgCharterMoney  int
+	CashPledge       int
+	RentCounter      int
+	LaundryCounter   int
+	MaintainCounter  int
+	CoverImg         string
+	SecondaryImg     string `gorm:"text"`
+	Status           string
+	CreatedTime      time.Time `gorm:"autoCreateTime"`
+	UpdatedTime      time.Time `gorm:"autoUpdateTime"`
 }
 
 func (c *DressCategory) FindById() error {
@@ -95,5 +95,5 @@ func (c *DressCategory) CountNormal() (count int64, err error) {
 
 // Updates 对一个指定了primary key的ORM 更新指定字段
 func (c *DressCategory) Updates() error {
-	return db.Db.Updates(c).Error
+	return db.Db.Save(c).Error
 }
